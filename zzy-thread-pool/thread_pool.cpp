ec08794fffb4b4cb6106e6ab9da19317f4679635
ZZY <zhengzhiyun2011@outlook.com>
2024-02-02 16:27:02 +0800

    增加测试+重构+修复bug：添加了单元测试；修复了wait_all无法等待任务全部执行完毕和add_loop_n在n < one_task_processing_number时waiter无效的2个bug；将类中的函数的声明与实现差分。

--- zzy-thread-pool/thread_pool.cpp
+++ zzy-thread-pool/thread_pool.cpp
@@ -3,16 +3,88 @@ using std::memory_order_relaxed;
 using std::memory_order_release;
 using std::cv_status;
 using std::function;
+using std::lock_guard;
 using std::mutex;
+using std::size_t;
+using std::thread;
 using std::unique_lock;
+using std::max;
 #ifdef _MSC_VER
 #pragma warning(disable : 4455)
 using std::operator""s;
 #pragma warning(default : 4455)
 #endif
+namespace chrono = std::chrono;
 namespace this_thread = std::this_thread;
 
 namespace ztools {
+    LoopWaiter::Wrapper::Wrapper(size_t tasks_count)
+        : count(tasks_count) {}
+
+    LoopWaiter::LoopWaiter() noexcept
+        : m_status(nullptr) {}
+
+    LoopWaiter::LoopWaiter(size_t tasks_number)
+        : m_status(std::make_shared<Wrapper>(tasks_number)) {}
+
+    ThreadPool::ThreadPool(
+        unsigned min_threads_number,
+        unsigned max_threads_number,
+        std::chrono::steady_clock::duration timeout_len
+    )
+        : m_threads_number(0)
+        , m_free_threads_number(0)
+        , m_added_tasks_number(0)
+        , m_finished_tasks_number(0)
+        , m_stop(false)
+        , m_min_threads_number(min_threads_number)
+        , m_max_threads_number(max_threads_number)
+        , m_timeout_len(timeout_len)
+    {
+        using std::thread;
+        using std::ref;
+
+        for (unsigned i = 0; i < min_threads_number; ++i) {
+            create_thread();
+        }
+    }
+
+    ThreadPool::ThreadPool()
+        : ThreadPool(
+            max(thread::hardware_concurrency() / 2, 1U),
+            thread::hardware_concurrency()
+        ) {}
+
+    ThreadPool::ThreadPool(chrono::steady_clock::duration timeout_len)
+        : ThreadPool(
+            max(thread::hardware_concurrency() / 2, 1U),
+            thread::hardware_concurrency(),
+            timeout_len
+        ) {}
+
+    ThreadPool::~ThreadPool()
+    {
+
+        {
+            lock_guard<mutex> lock(m_mutex);
+            m_stop = true;
+        }
+
+        m_scheduler.notify_all();
+
+        while (m_threads_number > 0) {
+            this_thread::yield();
+        }
+    }
+
+    void ThreadPool::wait_all()
+    {
+        while (!(m_tasks.empty() && m_added_tasks_number.load(memory_order_relaxed) ==
+            m_finished_tasks_number.load(memory_order_relaxed))) {
+            this_thread::yield();
+        }
+    }
+
     void ThreadPool::executor_func(ThreadPool& pool) noexcept
     {
         for (;;) {
@@ -55,6 +127,7 @@ namespace ztools {
             pool.m_free_threads_number.fetch_sub(1, memory_order_relaxed);
             task();
             pool.m_free_threads_number.fetch_add(1, memory_order_release);
+            pool.m_finished_tasks_number.fetch_add(1, memory_order_relaxed);
         }
     }
 }
